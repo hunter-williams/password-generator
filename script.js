efb45a76a084cbbbcd43a0c50ad3055f6db01156
hunter-williams <dragynson@yahoo.com>
2022-07-24 15:50:23 -0700

    user response

--- script.js
+++ script.js
@@ -7,11 +7,41 @@ const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","
 const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 const numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const specialCharecters = ["`","!","@","#","$","%","^","&","*","(",")","_","+","-","=","\\","[","]","{","}",";","'",":","|",".","<",">","/","?","~"]
-var length =  128
 //  hold user response
-var criteria = [lowerCase, upperCase, numeric, specialCharecters]
+var userresponseLength =  0
+var criteria = []
 
-console.log(criteria)
+
+if (confirm("Do you want lowercase?")){
+  criteria.push(lowerCase)
+}
+if (confirm("Do you want uppercase?")){
+  criteria.push(upperCase)
+}
+if (confirm("Do you want numbers?")){
+  criteria.push(numeric)
+}
+if (confirm("Do you want special characters?")){
+  criteria.push(specialCharecters)
+}
+
+console.log("criteria :", criteria)
+// prompt user to choose length, if they answer invalid make them choose again. if they choose wrong will force closes number.
+var firstResponse = prompt("Choose length of password between 8 and 128 characters")
+if (firstResponse < 128 && firstResponse > 8){
+  userresponseLength = firstResponse;
+} else if (firstResponse > 128 || firstResponse < 8){
+    var secondResponse = prompt("Invalid length, choose between 8 and 128");
+    // check second answer
+    if (secondResponse < 128 && secondResponse > 8){
+      userresponseLength = secondResponse;
+    } else if (secondResponse > 128){
+      userresponseLength = 128;
+    } else if (secondResponse < 8){
+      userresponseLength = 8;
+    }
+} 
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -23,14 +53,17 @@ function generatePassword (){
   var criteriaIndex = ""
   var charecterIndex  = ""
   // generate random indexs while password length is less than user response
-  for (i =  0; i < length;  i++){
+  for (i =  0; i < userresponseLength;  i++){
     // generate new random index
     criteriaIndex = Math.floor(Math.random() * criteria.length);
     charecterIndex  = Math.floor(Math.random() * criteria[criteriaIndex].length);
     // add to password
-    console.log(criteria[criteriaIndex][charecterIndex])
+    // console.log(criteria[criteriaIndex][charecterIndex])
     password+=(criteria[criteriaIndex][charecterIndex])
   }
+  console.log("password :", password)
+  console.log("password length :", password.length)
+
   return  password
 }
 
